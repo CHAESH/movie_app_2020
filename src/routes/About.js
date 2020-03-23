ed340dfd35eb47f794ee94785c368155b652973a
chaesh <csh1708@gmail.com>
2020-03-23 20:34:58 +0900

    #2. Routing

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About(props){
+    console.log(props);
+    return (
+        <div className="about__container">
+            <span>
+                "This website is just Movie List WebSite"
+            </span>
+        </div>   
+    );
+}
+
+export default About;
